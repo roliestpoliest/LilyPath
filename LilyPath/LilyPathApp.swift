cd8feaef02873b7b09342cbfc9220fb0c7015676
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2024-09-29 15:55:56 -0500

    Initial Commit

---
+++ LilyPath/LilyPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilyPathApp.swift
+//  LilyPath
+//
+//  Created by Carolyn Heron on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct LilyPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
