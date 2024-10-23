e44e8c0b2e24d939fdea8fd6adc91fa531614a76
Chelsea Nguyen <100812314+sheepsgarden@users.noreply.github.com>
2024-10-23 04:39:31 -0500

    feat(stats): implement PlantStatsModel and UserPlantManager and add lastWateredDate to UserPlantModel

---
+++ LilyPath/Models/PlantStatsModel.swift
@@ -0,0 +1,66 @@
+//
+//  PlantStatsModel.swift
+//  LilyPath
+//
+//  Created by Chelsea Nguyen on 10/23/24.
+//
+
+import Foundation
+
+class PlantStatsModel: Identifiable, ObservableObject {
+    let id: String
+    let icon: Icon
+    let plant: String
+    let action: String
+    @Published var count: Int
+    
+    init(id: String, icon: Icon, plant: String, action: String, count: Int = 0) {
+        self.id = id
+        self.icon = icon
+        self.plant = plant
+        self.action = action
+        self.count = count
+    }
+    
+    func description(for timePeriod: TimePeriod) -> String {
+        let value = formatNumberWithCommas(count)
+        return "\(value) \(detail(for: value))"
+    }
+    
+    private func detail(for value: String) -> String {
+        "\(unitWithPluralSuffix(for: value)) \(action)"
+    }
+    
+    private func unitWithPluralSuffix(for value: String) -> String {
+        "\(plant)\(value == "1" ? "" : "s")"
+    }
+}
+
+extension PlantStatsModel {
+    static let seedsPlanted = PlantStatsModel(
+        id: "seedsPlanted",
+        icon: .newPlant,
+        plant: "seed",
+        action: "planted"
+    )
+    
+    static let plantsCompleted = PlantStatsModel(
+        id: "plantsCompleted",
+        icon: .garden,
+        plant: "plant",
+        action: "completed"
+    )
+    
+    static let plantsWatered = PlantStatsModel(
+        id: "plantsWatered",
+        icon: .waterDrop,
+        plant: "plant",
+        action: "watered"
+    )
+    
+    static let allStats: [PlantStatsModel] = [
+        seedsPlanted,
+        plantsCompleted,
+        plantsWatered,
+    ]
+}
--- LilyPath/Models/UserPlantModel.swift
+++ LilyPath/Models/UserPlantModel.swift
@@ -7,7 +7,104 @@
 
 import SwiftUI
 
-class UserPlantModel:Identifiable {
+class UserPlantManager: ObservableObject {
+    static let shared = UserPlantManager()
+
+    @Published var userPlants: [UserPlantModel] = []
+
+    private init() {
+        // TODO: replace with actual user plants
+        userPlants = [
+            UserPlantModel(
+                basePlant: .peony,
+                completionDate: Calendar.current.date(byAdding: .day, value: -3, to: Date()),
+                currentStage: 1,
+                stepsCollected: 1000,
+                lastWateredDate: Calendar.current.date(byAdding: .day, value: -8, to: Date()),
+                status: .completed
+            ),
+            UserPlantModel(
+                basePlant: .lily,
+                completionDate: Date(),
+                currentStage: 4,
+                stepsCollected: 1000,
+                lastWateredDate: Date(),
+                status: .completed
+            )
+        ]
+    }
+
+    func updatePlantStats(for timePeriod: TimePeriod) {
+        PlantStatsModel.seedsPlanted.count =
+            filteredPlants(for: "seedsPlanted", within: timePeriod).count
+        PlantStatsModel.plantsCompleted.count =
+            filteredPlants(for: "plantsCompleted", within: timePeriod).count
+        PlantStatsModel.plantsWatered.count =
+            filteredPlants(for: "plantsWatered", within: timePeriod).count
+    }
+
+    func filteredPlants(for plantStat: String, within timePeriod: TimePeriod) -> [UserPlantModel] {
+        let now = Date()
+        let calendar = Calendar.current
+        let startDate: Date
+
+        switch timePeriod {
+        case .daily:
+            startDate = calendar.startOfDay(for: now)
+        case .weekly:
+            startDate = calendar.date(
+                from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: now)
+            ) ?? now
+        case .monthly:
+            startDate = calendar.date(
+                from: calendar.dateComponents([.year, .month], from: now)
+            ) ?? now
+        }
+
+        let filtered = userPlants.filter { plant in
+            switch plantStat {
+            case "seedsPlanted":
+                return plant.plantDate >= startDate
+            case "plantsCompleted":
+                return plant.completionDate != nil && plant.completionDate! >= startDate
+            case "plantsWatered":
+                return plant.lastWateredDate != nil && plant.lastWateredDate! >= startDate
+            default:
+                return false
+            }
+        }
+
+        return filtered.sorted {
+            let calendar = Calendar.current
+            
+            let firstDate = relevantDate(for: $0, stat: plantStat)
+                .map { date in calendar.startOfDay(for: date) } ?? Date.distantPast
+            let secondDate = relevantDate(for: $1, stat: plantStat)
+                .map { date in calendar.startOfDay(for: date) } ?? Date.distantPast
+
+            if firstDate == secondDate {
+                return $0.basePlant.species < $1.basePlant.species
+            } else {
+                return firstDate < secondDate
+            }
+        }
+    }
+
+    private func relevantDate(for plant: UserPlantModel, stat: String) -> Date? {
+        switch stat {
+        case "seedsPlanted":
+            return plant.plantDate
+        case "plantsCompleted":
+            return plant.completionDate
+        case "plantsWatered":
+            return plant.lastWateredDate
+        default:
+            return nil
+        }
+    }
+}
+
+class UserPlantModel: Identifiable {
     let id = UUID()
     let basePlant: BasePlantModel
     var plantDate: Date
@@ -15,48 +112,71 @@ class UserPlantModel:Identifiable {
     var currentStage: Int
     var stepsCollected: Int
     var watersCollected: Int
+    var lastWateredDate: Date?
     var numberOfRevives: Int
     var isCurrent: Bool
+    var status: PlantStatus
+
     var overallProgress: Double {
-        guard currentStage > 0, currentStage <= basePlant.stageStepGoals.count else {
+        guard currentStage > 0, currentStage <= basePlant.stageStepGoals.count
+        else {
             return 0
         }
         return Double(stepsCollected) / Double(basePlant.stepGoal)
     }
+
     var stepsInCurrentStage: Int {
-        guard currentStage > 1, currentStage <= basePlant.stageStepGoals.count else {
+        guard currentStage > 1, currentStage <= basePlant.stageStepGoals.count
+        else {
             return stepsCollected
         }
-        let previousStagesSteps = basePlant.stageStepGoals.prefix(currentStage - 1).reduce(0, +)
+        let previousStagesSteps = basePlant.stageStepGoals.prefix(
+            currentStage - 1
+        ).reduce(0, +)
         return stepsCollected - previousStagesSteps
     }
+
     var currentStageGoal: Int {
-        guard currentStage > 0, currentStage <= basePlant.stageStepGoals.count else {
+        guard currentStage > 0, currentStage <= basePlant.stageStepGoals.count
+        else {
             return -1
         }
         return basePlant.stageStepGoals[currentStage - 1]
     }
+
     var currentImage: String {
-        guard currentStage > 0, currentStage <= basePlant.stageImages.count else {
+        guard currentStage > 0, currentStage <= basePlant.stageImages.count
+        else {
             return "Wilt"
         }
         return basePlant.stageImages[currentStage - 1]
     }
+
     enum PlantStatus: String {
         case growing = "Growing"
         case completed = "Completed"
         case wilted = "Wilted"
-        //case revived = "Revived"
     }
-    var status: PlantStatus
-    
-    init(basePlant: BasePlantModel, plantDate: Date = Date(), completionDate: Date? = nil, currentStage: Int = 1, stepsCollected: Int = 0, watersCollected: Int = 0, numberOfRevives: Int = 0, isCurrent: Bool = true, status: PlantStatus = .growing) {
+
+    init(
+        basePlant: BasePlantModel,
+        plantDate: Date = Date(),
+        completionDate: Date? = nil,
+        currentStage: Int = 1,
+        stepsCollected: Int = 0,
+        watersCollected: Int = 0,
+        lastWateredDate: Date? = nil,
+        numberOfRevives: Int = 0,
+        isCurrent: Bool = true,
+        status: PlantStatus = .growing
+    ) {
         self.basePlant = basePlant
         self.plantDate = plantDate
         self.completionDate = completionDate
         self.currentStage = currentStage
         self.stepsCollected = stepsCollected
         self.watersCollected = watersCollected
+        self.lastWateredDate = lastWateredDate
         self.numberOfRevives = numberOfRevives
         self.isCurrent = isCurrent
         self.status = status
