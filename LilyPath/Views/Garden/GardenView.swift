2d788a0201c2e14daff285dee25d8d3ae8418d89
Chelsea Nguyen <100812314+sheepsgarden@users.noreply.github.com>
2024-10-23 17:12:58 -0500

    feat(garden): add YourGardenView and add nav link to PlantGalleryView

--- LilyPath/Views/Garden/GardenView.swift
+++ LilyPath/Views/Garden/GardenView.swift
@@ -8,13 +8,77 @@
 import SwiftUI
 
 struct GardenView: View {
+    @ObservedObject private var userPlantManager = UserPlantManager.shared
+
     var body: some View {
-        VStack {
-            Text("Garden View!")
+        NavigationStack {
+            VStack {
+                GardenNavigationLink(
+                    title: "Your Garden",
+                    destination: PlantGalleryView())
+
+                YourGardenView()
+
+                // TODO: replace with current plant popup
+                GardenNavigationLink(
+                    title: "Current Plant", destination: GardenView())
+
+                Spacer()
+            }
+            .background(Color.mainBackground)
+        }
+    }
+}
+
+struct YourGardenView: View {
+    @ObservedObject private var userPlantManager = UserPlantManager.shared
+
+    private let columns: [GridItem] = Array(repeating: GridItem(.flexible(), spacing: 10), count: 6)
+    
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 10) {
+            ForEach(userPlantManager.getUserPlantsSorted(), id: \.id) { plant in
+                Image(plant.currentImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 60)
+            }
+        }
+        .padding(20)
+        .frame(maxWidth: .infinity, minHeight: 200)
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.customBrown)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .inset(by: 3)
+                        .stroke(Color.customPink, lineWidth: 6)
+                )
+        )
+    }
+}
+
+struct GardenNavigationLink<Destination: View>: View {
+    let title: String
+    let destination: Destination
+
+    var body: some View {
+        NavigationLink(destination: destination) {
+            HStack {
+                ViewTitle(title: title)
+
+                Spacer()
+
+                Image(systemName: "chevron.right")
+                    .font(.viewTitle)
+                    .foregroundColor(Color.customBrown)
+            }
         }
     }
 }
 
 #Preview {
     GardenView()
+        .background(Color.mainBackground)
+        .padding(.horizontal, 30)
 }
--- LilyPath/Views/Garden/PlantGalleryView.swift
+++ LilyPath/Views/Garden/PlantGalleryView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct PlantGalleryView: View {
-    var plantGallery: [UserPlantModel]
+    @ObservedObject private var userPlantManager = UserPlantManager.shared
     let columns = [
-        GridItem(.flexible(), spacing: 50),
-        GridItem(.flexible(), spacing: 50)
+        GridItem(.flexible(), spacing: 40),
+        GridItem(.flexible(), spacing: 40)
     ]
 
     var body: some View {
@@ -20,23 +20,25 @@ struct PlantGalleryView: View {
             
             ScrollView(showsIndicators: false) {
                 LazyVGrid(columns: columns, spacing: 30) {
-                    ForEach(plantGallery) { plant in
+                    ForEach(userPlantManager.getUserPlantsSorted()) { plant in
                         PlantGalleryCard(userPlantModel: plant)
                     }
                 }
             }
+            .shadow(radius: ShadowConstants.radius, y: ShadowConstants.yOffset)
         }
+        .background(Color.mainBackground)
     }
 }
 
 #Preview {
-    PlantGalleryView(plantGallery: [
+    PlantGalleryView(/*plantGallery: [
         UserPlantModel(basePlant: BasePlantModel.peony, currentStage: 1, stepsCollected: 50),
         UserPlantModel(basePlant: BasePlantModel.peony, currentStage: 2, stepsCollected: 200),
         UserPlantModel(basePlant: BasePlantModel.lily, currentStage: 2, stepsCollected: 200),
         UserPlantModel(basePlant: BasePlantModel.lotus, currentStage: 4, stepsCollected: 1220),
         UserPlantModel(basePlant: BasePlantModel.lavender, currentStage: 2, stepsCollected: 200)
-    ])
+    ]*/)
         .padding(.horizontal, 30)
         .background(Color.mainBackground)
 
