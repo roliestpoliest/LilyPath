b93eeb3cc3fc06b68819897abebdb731543b797d
Chelsea Nguyen <100812314+sheepsgarden@users.noreply.github.com>
2024-10-24 00:16:34 -0500

    style(plantGallery): change stage to a circle number icon and adjust grid

--- LilyPath/Views/Garden/PlantGalleryCard.swift
+++ LilyPath/Views/Garden/PlantGalleryCard.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct PlantGalleryCard: View {
     var userPlantModel: UserPlantModel
-    
+
     var body: some View {
         VStack {
             VStack {
@@ -17,65 +17,83 @@ struct PlantGalleryCard: View {
                     Rectangle()
                         .fill(Color.customBrown)
                         .cornerRadius(15)
-                    
+
                     Spacer()
-                    
+
                     VStack {
                         Spacer()
                         ZStack {
-                            UnevenRoundedRectangle(topLeadingRadius: 8, bottomLeadingRadius: 0, bottomTrailingRadius: 0, topTrailingRadius: 8)
-                                .fill(Color.lightBlue)
-                                .frame(width: 125, height: 125)
+                            UnevenRoundedRectangle(
+                                topLeadingRadius: 8, bottomLeadingRadius: 0,
+                                bottomTrailingRadius: 0, topTrailingRadius: 8
+                            )
+                            .fill(Color.lightBlue)
+                            .frame(width: 125, height: 125)
                             Spacer()
                             Image(userPlantModel.currentImage)
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
-                            // .scaleEffect(0.45)
-                                .frame(width: 62, height: 62)
-                            
-                            Text("Stage \(userPlantModel.currentStage)")
-                                .font(.caption)
-                                .padding(.top, 100)
-                                .padding(.trailing, 70)
+                                .frame(width: 45, height: 65)
 
                         }
                         .padding(.top, 5)
-                        
+
                         Spacer()
-                        
-                        VStack {
+
+                        VStack(spacing: 5) {
                             Text(userPlantModel.basePlant.species)
 
-                                ProgressBar(value: Double(userPlantModel.stepsInCurrentStage), total: Double(userPlantModel.currentStageGoal))
+                            HStack(spacing: 5) {
+                                Image(
+                                    systemName:
+                                        "\(userPlantModel.currentStage).circle.fill"
+                                )
+                                .foregroundColor(.lightGreen)
 
-                                .padding(.horizontal, 15)
+                                ProgressBar(
+                                    value: Double(
+                                        userPlantModel.stepsInCurrentStage),
+                                    total: Double(
+                                        userPlantModel.currentStageGoal))
+                            }
+                            .padding(.horizontal, 10)
+
                         }
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
                         .font(Font.statsCard)
                         .foregroundStyle(.white)
                         .bold()
-                        
+
                         Spacer()
                     }
                 }
+                .frame(width: 150, height: 210)
             }
-            .frame(width: 150, height: 215)
         }
         .contentShape(Rectangle())
         .onTapGesture {
-            print("Plant Gallery Card \(userPlantModel.basePlant.species) tapped")
+            print(
+                "Plant Gallery Card \(userPlantModel.basePlant.species) tapped")
         }
     }
 }
 
 #Preview {
-    VStack{
-        PlantGalleryCard(userPlantModel: UserPlantModel(basePlant: BasePlantModel.buttercup, currentStage: 3, stepsCollected: [100, 200, 100].reduce(0,+)))
-        PlantGalleryCard(userPlantModel: UserPlantModel(basePlant: BasePlantModel.buttercup, currentStage: 3, stepsCollected: [100, 200, 100].reduce(0,+)))
-        PlantGalleryCard(userPlantModel: UserPlantModel(basePlant: BasePlantModel.lavender, currentStage: 5, stepsCollected: [110, 210, 310, 410, 0].reduce(0,+)))
+    VStack {
+        PlantGalleryCard(
+            userPlantModel: UserPlantModel(
+                basePlant: BasePlantModel.buttercup, currentStage: 3,
+                stepsCollected: [100, 200, 100].reduce(0, +)))
+        PlantGalleryCard(
+            userPlantModel: UserPlantModel(
+                basePlant: BasePlantModel.buttercup, currentStage: 3,
+                stepsCollected: [100, 200, 100].reduce(0, +)))
+        PlantGalleryCard(
+            userPlantModel: UserPlantModel(
+                basePlant: BasePlantModel.lavender, currentStage: 5,
+                stepsCollected: [110, 210, 310, 410, 0].reduce(0, +)))
     }
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .padding(.horizontal, 30)
     .background(Color.mainBackground)
-    
+
 }
--- LilyPath/Views/Garden/PlantGalleryView.swift
+++ LilyPath/Views/Garden/PlantGalleryView.swift
@@ -9,17 +9,14 @@ import SwiftUI
 
 struct PlantGalleryView: View {
     @ObservedObject private var userPlantManager = UserPlantManager.shared
-    let columns = [
-        GridItem(.flexible(), spacing: 40),
-        GridItem(.flexible(), spacing: 40)
-    ]
+    private let columns: [GridItem] = Array(repeating: GridItem(.flexible(), spacing: 40), count: 2)
 
     var body: some View {
         VStack{
             ViewTitle(title: "Plant Gallery")
             
             ScrollView(showsIndicators: false) {
-                LazyVGrid(columns: columns, spacing: 30) {
+                LazyVGrid(columns: columns, spacing: 40) {
                     ForEach(userPlantManager.getUserPlantsSorted()) { plant in
                         PlantGalleryCard(userPlantModel: plant)
                     }
@@ -32,13 +29,7 @@ struct PlantGalleryView: View {
 }
 
 #Preview {
-    PlantGalleryView(/*plantGallery: [
-        UserPlantModel(basePlant: BasePlantModel.peony, currentStage: 1, stepsCollected: 50),
-        UserPlantModel(basePlant: BasePlantModel.peony, currentStage: 2, stepsCollected: 200),
-        UserPlantModel(basePlant: BasePlantModel.lily, currentStage: 2, stepsCollected: 200),
-        UserPlantModel(basePlant: BasePlantModel.lotus, currentStage: 4, stepsCollected: 1220),
-        UserPlantModel(basePlant: BasePlantModel.lavender, currentStage: 2, stepsCollected: 200)
-    ]*/)
+    PlantGalleryView()
         .padding(.horizontal, 30)
         .background(Color.mainBackground)
 
