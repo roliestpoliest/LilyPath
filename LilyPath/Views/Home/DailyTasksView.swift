842bc5aa1e43ccc782984c48c37604d5a55241e8
Chelsea Nguyen <100812314+sheepsgarden@users.noreply.github.com>
2024-12-02 05:05:50 -0600

    chore(dailyTasks): reorganize code

--- LilyPath/Views/Home/DailyTasksView.swift
+++ LilyPath/Views/Home/DailyTasksView.swift
@@ -62,6 +62,44 @@ struct DailyTasksView: View {
             print("Failed to save new tasks: \(error)")
         }
     }
+    
+    private func generateRandomTasks() -> [TaskModel] {
+        func generateRandomGoal(for taskType: TaskType) -> Int {
+            let range = stride(
+                from: taskType.lowerBound,
+                through: taskType.upperBound,
+                by: taskType.incrementFactor
+            )
+            
+            return Array(range).randomElement() ?? taskType.lowerBound
+        }
+        
+        func calculateRewards(for taskType: TaskType, goal: Int) -> (
+            waterPoints: Int, gems: Int
+        ) {
+            let maxGoal = taskType.upperBound
+            let minGoal = taskType.lowerBound
+            
+            let progress = Double(goal - minGoal) / Double(maxGoal - minGoal)
+            
+            let waterPoints = Int(100 + progress * 100) * 10  // Min: 1000, Max: 2000, Increment: 10
+            let gems = Int(1 + progress * 3)  // Min: 1, Max: 4
+            
+            return (waterPoints, gems)
+        }
+        
+        return TaskType.allTasks.map { taskType in
+            let goal = generateRandomGoal(for: taskType)
+            let rewards = calculateRewards(for: taskType, goal: goal)
+            
+            return TaskModel(
+                type: taskType,
+                goal: goal,
+                waterPointReward: rewards.waterPoints,
+                gemReward: rewards.gems
+            )
+        }
+    }
 }
 
 #Preview {
