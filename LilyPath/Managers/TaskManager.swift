71c996c7846006920d4bf7c57e8a6a78e11e9178
Chelsea Nguyen <100812314+sheepsgarden@users.noreply.github.com>
2024-12-02 04:58:11 -0600

    chore(dailyTasks): remove debugging code

--- LilyPath/Managers/TaskManager.swift
+++ LilyPath/Managers/TaskManager.swift
@@ -5,19 +5,4 @@
 //  Created by Chelsea Nguyen on 10/23/24.
 //
 
-import Foundation
-
-class TaskManager: ObservableObject {
-    static let shared = TaskManager()
-    
-    @Published var tasks: [TaskModel]
-    
-    private init() {
-        self.tasks = generateRandomTasks()
-    }
-    
-    // TODO: generate randome tasks once a day
-    func generateNewTasks() {
-        self.tasks = generateRandomTasks()
-    }
-}
+// TODO: DELETE UNUSED FILE
--- LilyPath/Views/Home/DailyTasksView.swift
+++ LilyPath/Views/Home/DailyTasksView.swift
@@ -19,35 +19,6 @@ struct DailyTasksView: View {
             
             ViewTitle(title: "Daily Tasks")
             
-            // TODO: remove, using to generate for testing
-            HStack {
-                Button(action: {
-                    generateTasks()
-                }) {
-                    Text("Generate Tasks")
-                        .font(.customBody)
-                        .foregroundColor(.white)
-                        .padding(.vertical, 8)
-                        .padding(.horizontal, 16)
-                        .background(Color.customBrown)
-                        .cornerRadius(10)
-                }
-                
-                Group {
-                    if tasks.isEmpty {
-                        Text("No tasks available")
-                            .font(.customBody)
-                            .foregroundColor(.customBrown)
-                            .padding(.top, 20)
-                    } else {
-                        Text("Tasks: \(tasks.count)")
-                            .font(.customBody)
-                            .foregroundColor(.customBrown)
-                            .padding(.top, 20)
-                    }
-                }
-            }
-            
             ScrollView(showsIndicators: false) {
                 ForEach(tasks) { task in
                     TaskCard(task: task)
@@ -61,8 +32,6 @@ struct DailyTasksView: View {
         .background(Color.mainBackground)
         .onAppear {
             generateTasksIfNeeded()
-            print("Daily Tasks View appeared, checking for existing tasks...")
-            loadTasksFromDatabase()
         }
     }
     
@@ -84,7 +53,6 @@ struct DailyTasksView: View {
             modelContext.insert(task)
         }
 
-        print("New tasks generated")
         do {
             try modelContext.save()
             lastGeneratedDate = currentDate
@@ -92,46 +60,6 @@ struct DailyTasksView: View {
             print("New tasks saved successfully on", currentDate)
         } catch {
             print("Failed to save new tasks: \(error)")
-        }
-    }
-    
-    // TODO: remove, using to generate for testing
-    private func generateTasks() {
-        let currentDate = Calendar.current.startOfDay(for: Date())
-
-        for task in tasks {
-            modelContext.delete(task)
-        }
-
-        let newTasks = generateRandomTasks()
-        for task in newTasks {
-            task.timestamp = Date()
-            modelContext.insert(task)
-        }
-
-        print("New tasks generated")
-        do {
-            try modelContext.save()
-            lastGeneratedDate = currentDate
-            UserDefaults.standard.set(lastGeneratedDate, forKey: "lastGeneratedDate")
-            print("New tasks saved successfully on", currentDate)
-
-            let fetchDescriptor = FetchDescriptor<TaskModel>()
-            let savedTasks = try modelContext.fetch(fetchDescriptor)
-            print("Saved tasks in DB:", savedTasks.map { $0.id.uuidString })
-        } catch {
-            print("Failed to save new tasks: \(error)")
-        }
-    }
-    
-    // TODO: remove, using to generate for testing
-    private func loadTasksFromDatabase() {
-        do {
-            let fetchDescriptor = FetchDescriptor<TaskModel>(sortBy: [SortDescriptor(\.timestamp)])
-            let fetchedTasks = try modelContext.fetch(fetchDescriptor)
-            print("Loaded tasks from DB:", fetchedTasks.map { "\($0.id.uuidString) goal: \($0.goal)" })
-        } catch {
-            print("Failed to load tasks from DB:", error)
         }
     }
 }
