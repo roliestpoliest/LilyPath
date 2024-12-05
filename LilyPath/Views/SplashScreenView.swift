7f7a92c789dfdb63d0ba2220c9e85d4a2e3e34ee
Chelsea Nguyen <100812314+sheepsgarden@users.noreply.github.com>
2024-12-05 13:06:40 -0600

    feat: prompt HowToPlayView on user's first launch and add scale animation to SplashScreen

--- LilyPath/Views/SplashScreenView.swift
+++ LilyPath/Views/SplashScreenView.swift
@@ -9,37 +9,65 @@ import Foundation
 import SwiftUI
 
 struct SplashScreenView: View {
-    @State var isActive : Bool = false
-    @State private var size = 0.8
-    @State private var opacity = 0.5
-    
+    @State var isActive: Bool = false
+    @State private var showHowToPlay: Bool = false
+    @State private var scaleAmount: CGFloat = 1
+
     var body: some View {
         if isActive {
             ContentView()
         } else {
             VStack {
                 Spacer()
-                HStack{
+                HStack {
                     Text("LilyPath")
-                    .font(.viewTitle)
-                    .foregroundColor(.customBrown)
+                        .font(.viewTitle)
+                        .foregroundColor(.customBrown)
+
                     Image("Lily Stage 5")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(maxWidth: 50, maxHeight: 50)
-
                 }
                 Spacer()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .scaleEffect(scaleAmount)
             .background(Color.mainBackground)
             .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                    withAnimation {
-                        self.isActive = true
+                withAnimation(.easeOut(duration: 0.5)) {
+                    scaleAmount = 0.8
+                }
+                withAnimation(.easeIn(duration: 1.5).delay(0.5)) {
+                    scaleAmount = 1.8
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    if isFirstLaunch() {
+                        showHowToPlay = true
+                    } else {
+                        isActive = true
                     }
                 }
             }
+            .sheet(isPresented: $showHowToPlay, onDismiss: {
+                withAnimation {
+                    isActive = true
+                }
+            }) {
+                HowToPlayView(showSheet: $showHowToPlay)
+            }
+        }
+    }
+
+    private func isFirstLaunch() -> Bool {
+        let userDefaults = UserDefaults.standard
+        let hasLaunchedKey = "hasLaunchedBefore"
+
+        if userDefaults.bool(forKey: hasLaunchedKey) {
+            return false
+        } else {
+            userDefaults.set(true, forKey: hasLaunchedKey)
+            return true
         }
     }
 }
